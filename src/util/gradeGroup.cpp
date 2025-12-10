17f6fb48a4c17b791bdbe494d767bf15ef4c7bc4
LunaJang <lunajang@kakao.com>
2025-12-10 19:37:30 +0900

    change gradeGroup script

--- src/util/gradeGroup.cpp
+++ src/util/gradeGroup.cpp
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <regex>
 #include <cstdint>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -41,10 +44,12 @@ int gradeGroup(int argc, const char **argv, const Command &command) {
     LocalParameters &par = LocalParameters::getLocalInstance();
     setGradeGroupDefault(par);
     par.parseParameters(argc, argv, command, false, Parameters::PARSE_ALLOW_EMPTY, 0);
-    const string groupFileList = par.filenames[0];
+
+    const string groupFileList     = par.filenames[0];
     const string readGroupFileList = par.filenames[1];
-    const string mappingFileList = par.filenames[2];
-    const string taxonomy = par.filenames[3];
+    const string mappingFileList   = par.filenames[2];
+    const string queryNameFileList = par.filenames[3]; // NEW
+    const string taxonomy          = par.filenames[4];
 
     // Parse ranks
     vector<string> ranks;
@@ -55,201 +60,244 @@ int gradeGroup(int argc, const char **argv, const Command &command) {
     }
 
     // Load Taxonomy
-    string names = taxonomy + "/names.dmp";
-    string nodes = taxonomy + "/nodes.dmp";
+    string names  = taxonomy + "/names.dmp";
+    string nodes  = taxonomy + "/nodes.dmp";
     string merged = taxonomy + "/merged.dmp";
 
-    // Load mapping file names
-    ifstream mappingFileListFile;
-    mappingFileListFile.open(mappingFileList);
     string eachLine;
+
+    // Load mapping file names
+    ifstream mappingFileListFile(mappingFileList);
     vector<string> mappingFileNames;
     if (mappingFileListFile.is_open()) {
         while (getline(mappingFileListFile, eachLine)) {
-            mappingFileNames.push_back(eachLine);
+            if (!eachLine.empty())
+                mappingFileNames.push_back(eachLine);
         }
     } else {
         cerr << "Cannot open file for mapping file list" << endl;
     }
-    cout << "Answer sheet loaded" << endl;
+    cout << "Answer sheet list loaded" << endl;
 
     // Load group file names
-    ifstream groupFileListFile;
-    groupFileListFile.open(groupFileList);
+    ifstream groupFileListFile(groupFileList);
     vector<string> groupFileNames;
     if (groupFileListFile.is_open()) {
         while (getline(groupFileListFile, eachLine)) {
-            groupFileNames.push_back(eachLine);
+            if (!eachLine.empty())
+                groupFileNames.push_back(eachLine);
         }
     } else {
-        cerr << "Cannot open file for read group file list" << endl;
+        cerr << "Cannot open file for group file list" << endl;
     }
-    cout << "Grouping results loaded" << endl;
+    cout << "Grouping results list loaded" << endl;
 
     // Load readgroup file names
-    ifstream readGroupFileListFile;
-    readGroupFileListFile.open(readGroupFileList);
+    ifstream readGroupFileListFile(readGroupFileList);
     vector<string> readGroupFileNames;
     if (readGroupFileListFile.is_open()) {
         while (getline(readGroupFileListFile, eachLine)) {
-            readGroupFileNames.push_back(eachLine);
+            if (!eachLine.empty())
+                readGroupFileNames.push_back(eachLine);
         }
     } else {
         cerr << "Cannot open file for read group file list" << endl;
     }
-    cout << "Read-group mapping results loaded" << endl;
+    cout << "Read-group mapping results list loaded" << endl;
+
+    // NEW: Load query-name file names
+    ifstream queryNameFileListFile(queryNameFileList);
+    vector<string> queryNameFileNames;
+    if (queryNameFileListFile.is_open()) {
+        while (getline(queryNameFileListFile, eachLine)) {
+            if (!eachLine.empty())
+                queryNameFileNames.push_back(eachLine);
+        }
+    } else {
+        cerr << "Cannot open file for query name file list" << endl;
+    }
+    cout << "Query-name list files loaded" << endl;
 
     size_t numberOfFiles = groupFileNames.size();
-    vector<GradeResult> results;
-    results.resize(numberOfFiles);
+    vector<GradeResult> results(numberOfFiles);
 
 #ifdef OPENMP
     omp_set_num_threads(par.threads);
 #endif
 
-#pragma omp parallel default(none), shared(results, ranks, numberOfFiles, mappingFileNames, groupFileNames,readGroupFileNames,\
-par, cout, cerr, names, nodes, merged)
+#pragma omp parallel default(none), \
+    shared(results, ranks, numberOfFiles, mappingFileNames, groupFileNames, \
+           readGroupFileNames, queryNameFileNames, \
+           par, cout, cerr, names, nodes, merged)
     {
-        // Grade each file
         unordered_map<string, int> assacc2taxid;
-        vector<int> classList;
-        vector<float> scores;
         string mappingFile;
         string groupFileName;
         string readGroupFileName;
+        string queryNameFileName;
 
         vector<string> ranks_local = ranks;
 
         TaxonomyWrapper ncbiTaxonomy(names, nodes, merged, false);
         cout << "Taxonomy loaded" << endl;
 
+        regex regexName("(GC[AF]_[0-9]+\\.[0-9]+)");
+
 #pragma omp for schedule(dynamic)
         for (size_t i = 0; i < numberOfFiles; ++i) {
-            // Initialize
             assacc2taxid.clear();
-            mappingFile = mappingFileNames[i];
-            groupFileName = groupFileNames[i];
+            mappingFile       = mappingFileNames[i];
+            groupFileName     = groupFileNames[i];
             readGroupFileName = readGroupFileNames[i];
+            queryNameFileName = queryNameFileNames[i];
 
-            // Load the mapping file (answer sheet) (accession to taxID)
-            string key, value;
-            ifstream map;
-            map.open(mappingFile);
-            if (map.is_open()) {
-                while (getline(map, key, '\t')) {
-                    getline(map, value, '\n');
-                    // remove version number
-                    size_t pos = key.find('.');
-                    if (pos != string::npos) {
-                        key = key.substr(0, pos);
+            {
+                string key, value;
+                ifstream map(mappingFile);
+                if (map.is_open()) {
+                    while (getline(map, key, '\t')) {
+                        if (!getline(map, value, '\n')) break;
+                        // remove version number
+                        size_t pos = key.find('.');
+                        if (pos != string::npos) {
+                            key = key.substr(0, pos);
+                        }
+                        assacc2taxid[key] = stoi(value);
                     }
-                    assacc2taxid[key] = stoi(value);
+                } else {
+                    cerr << "Cannot open file for answer: " << mappingFile << endl;
                 }
-            } else {
-                cout << "Cannot open file for answer" << endl;
             }
-            map.close();
-            cout << "Mapping loaded" << endl;
+            cout << "Mapping loaded: " << mappingFile << endl;
 
-            regex regexName("(GC[AF]_[0-9]+\\.[0-9]+)");
+            vector<int> queryTaxid;          // index 0 -> query index 1
+            {
+                ifstream qf(queryNameFileName);
+                string qline;
+                while (getline(qf, qline)) {
+                    if (qline.empty()) {
+                        queryTaxid.push_back(-1);
+                        continue;
+                    }
+                    if (qline[0] == '>')
+                        qline = qline.substr(1);
 
-            // Load group results
+                    string id = qline;
+
+                    if (par.testType == "gtdb") {
+                        smatch m;
+                        if (regex_search(qline, m, regexName)) {
+                            id = m[0];
+                            size_t pos = id.find('.');
+                            if (pos != string::npos)
+                                id = id.substr(0, pos);
+                        } else {
+                            queryTaxid.push_back(-1);
+                            continue;
+                        }
+                    } else if (par.testType == "hiv" || par.testType == "hiv-ex") {
+                        size_t pos = id.find('_');
+                        if (pos != string::npos) id = id.substr(0, pos);
+                    } else if (par.testType == "cami" || par.testType == "cami-long" || par.testType == "cami-euk") {
+                        size_t pos = id.find('/');
+                        if (pos != string::npos) id = id.substr(0, pos);
+                    } else if (par.testType == "over") {
+                        smatch m;
+                        if (regex_search(qline, m, regexName)) {
+                            id = m[0];
+                        }
+                    }
+
+                    auto it = assacc2taxid.find(id);
+                    if (it == assacc2taxid.end()) {
+                        queryTaxid.push_back(-1);
+                    } else {
+                        queryTaxid.push_back(it->second);
+                    }
+                }
+            }
+            cout << "Query names loaded: " << queryNameFileName 
+                 << " (size=" << queryTaxid.size() << ")" << endl;
+
             string groupResultLine;
-            ifstream groupFile;
-            groupFile.open(groupFileName);            
+            ifstream groupFile(groupFileName);            
 
-            smatch assacc1;
             size_t numGroup = 0; 
             size_t numReadsInGroup = 0; 
             unordered_map<int, vector<int>> group2taxs;
 
-            while (getline(groupFile, groupResultLine, '\n')) {
-                if (groupResultLine.empty()) {
+            while (getline(groupFile, groupResultLine)) {
+                if (groupResultLine.empty())
                     continue;
-                }
-                vector<string> fields = Util::split(groupResultLine, "\t");  
-                for (int i = 1; i < fields.size(); i++){
-                    string id = fields[i];
-                    if (par.testType == "gtdb") {
-                        regex_search(id, assacc1, regexName);
-                        id = assacc1[0];
-                        // remove version number
-                        size_t pos = id.find('.');
-                        if (pos != string::npos) {
-                            id = id.substr(0, pos);
-                        }
-                    } else if (par.testType == "hiv" || par.testType == "hiv-ex") {
-                        size_t pos = id.find('_');
-                        id = id.substr(0, pos);
-                    } else if (par.testType == "cami" || par.testType == "cami-long" || par.testType == "cami-euk") {
-                        size_t pos = id.find('/');
-                        id = id.substr(0, pos);
-                    } else if (par.testType == "over") {
-                        regex_search(id, assacc1, regexName);
-                        id = assacc1[0];
+
+                vector<string> fields = Util::split(groupResultLine, "\t");
+                if (fields.size() < 2)
+                    continue;
+
+                int groupId = stoi(fields[0]);
+
+                for (size_t j = 1; j < fields.size(); j++) {
+                    int qIdx = stoi(fields[j]);  // 1-based
+                    size_t vecIdx = static_cast<size_t>(qIdx - 1);
+
+                    int taxid = queryTaxid[vecIdx];
+                    if (taxid < 0) {
+                        continue;
                     }
-                    group2taxs[stoi(fields[0])].push_back(assacc2taxid[id]);   
-                    numReadsInGroup++;                    
+
+                    group2taxs[groupId].push_back(taxid);
+                    numReadsInGroup++;
                 }
                 numGroup++;
             }
             groupFile.close();
-            
 
-            // Load read-group results
             string readGroupResultLine;
-            ifstream readGroupFile;
-            readGroupFile.open(readGroupFileName);
+            ifstream readGroupFile(readGroupFileName);
 
-            smatch assacc2;
-            size_t numReads = 0;    
+            size_t numReads = 0;
             unordered_map<string, unordered_map<int, vector<int>>> tax2groups;
 
-            while (getline(readGroupFile, readGroupResultLine, '\n')) {
-                if (readGroupResultLine.empty()) {
+            while (getline(readGroupFile, readGroupResultLine)) {
+                if (readGroupResultLine.empty())
                     continue;
-                }
-                
-                // Parse grouping result
-                vector<string> fields = Util::split(readGroupResultLine, "\t");    
-                if (stoi(fields[1]) != 0){
-                string id = fields[0];
-                    if (par.testType == "gtdb") {
-                        regex_search(id, assacc2, regexName);
-                        id = assacc2[0];
-                        // remove version number
-                        size_t pos = id.find('.');
-                        if (pos != string::npos) {
-                            id = id.substr(0, pos);
-                        }
-                    } else if (par.testType == "hiv" || par.testType == "hiv-ex") {
-                        size_t pos = id.find('_');
-                        id = id.substr(0, pos);
-                    } else if (par.testType == "cami" || par.testType == "cami-long" || par.testType == "cami-euk") {
-                        size_t pos = id.find('/');
-                        id = id.substr(0, pos);
-                    } else if (par.testType == "over") {
-                        regex_search(id, assacc2, regexName);
-                        id = assacc2[0];
-                    }
 
-                    for (const string &rank: ranks_local) {
-                        tax2groups[rank][ncbiTaxonomy.getTaxIdAtRank(assacc2taxid[id], rank)].push_back(stoi(fields[1]));     
-                    }
-                }
+                vector<string> fields = Util::split(readGroupResultLine, "\t");
+                if (fields.size() < 2)
+                    continue;
+
+                int qIdx = stoi(fields[0]);   // 1-based query index
+                int gId  = stoi(fields[1]);   // group id
+
                 numReads++;
 
+                if (gId == 0)
+                    continue;
+
+                size_t vecIdx = static_cast<size_t>(qIdx - 1);
+                if (vecIdx >= queryTaxid.size() || qIdx <= 0) {
+                    cerr << "Warning: qIdx " << qIdx << " out of range in read-group file "
+                         << readGroupFileName << endl;
+                    continue;
+                }
+
+                int taxid = queryTaxid[vecIdx];
+                if (taxid < 0)
+                    continue;
+
+                for (const string &rank : ranks_local) {
+                    int taxAtRank = ncbiTaxonomy.getTaxIdAtRank(taxid, rank);
+                    tax2groups[rank][taxAtRank].push_back(gId);
+                }
             }
             readGroupFile.close();
-            
-            // Score the groupinh
+
             for (const string &rank: ranks_local) {
                 countAtRank_CAMI(tax2groups[rank], group2taxs, ncbiTaxonomy,
                                  results[i].countsAtRanks[rank], rank);
                 results[i].countsAtRanks[rank].calculate();                 
             }
 
-            // Print Grade Result of each file
             cout << readGroupFileName << endl;
             cout << "The number of reads: " << numReads << endl;
             cout << "The number of groups: " << numGroup << endl;
@@ -259,17 +307,16 @@ par, cout, cerr, names, nodes, merged)
 
     cout << "Rank\t";
     for (size_t i = 0; i < results.size(); i++) {
-        // cout << "Purity\tRecall\tF1\tARI\t";
         cout << "Purity\tRecall\tF1\t";
     }
     cout << endl;
+
     for (const string &rank: ranks) {
         cout << rank << "\t";
         for (auto & result : results) {
-            // cout << result.countsAtRanks[rank].purity << "\t" << result.countsAtRanks[rank].recall
-            //      << "\t" << result.countsAtRanks[rank].f1 << "\t" << result.countsAtRanks[rank].ARI << "\t";
-            cout << result.countsAtRanks[rank].purity << "\t" << result.countsAtRanks[rank].recall
-                 << "\t" << result.countsAtRanks[rank].f1 << "\t";
+            cout << result.countsAtRanks[rank].purity << "\t"
+                 << result.countsAtRanks[rank].recall << "\t"
+                 << result.countsAtRanks[rank].f1 << "\t";
         }
         cout << endl;
     }
@@ -306,7 +353,6 @@ void countAtRank_CAMI(const unordered_map<int, vector<int>>& tax2groupsAtRank,
             freq[groupId]++;
         }
         
-        float groupRecall = 0;
         float numMajorGroupinTax = 0;
         for (auto &p : freq) {
             numMajorGroupinTax = max(numMajorGroupinTax, p.second);
@@ -314,46 +360,5 @@ void countAtRank_CAMI(const unordered_map<int, vector<int>>& tax2groupsAtRank,
         numMajorGroup += numMajorGroupinTax;
         numTaxGroups += groupIds.size();
     }
-    count.recall = numMajorGroup / numTaxGroups;
-
-    auto comb2 = [](long long n) -> long long { return (n < 2) ? 0LL : (n * (n - 1)) / 2; };
-
-    // contingency: groupId -> (labelAtRank -> count)
-    // std::unordered_map<int, std::unordered_map<int, long long>> contingency;
-    // std::unordered_map<int, long long> groupSizes;  // a_i
-    // std::unordered_map<int, long long> labelSizes;  // b_j
-    // long long N = 0;
-
-    // for (const auto& [gid, memberTaxs] : group2taxs) {
-    //     auto& row = contingency[gid];
-    //     long long gi = 0;
-    //     for (int t : memberTaxs) {
-    //         int lab = ncbiTaxonomy.getTaxIdAtRank(t, rank);
-    //         if (lab <= 0) continue;                 // unknown 제외(규칙 일관)
-    //         ++row[lab];
-    //         ++labelSizes[lab];
-    //         ++gi;
-    //     }
-    //     groupSizes[gid] += gi;
-    //     N += gi;
-    // }
-
-    // long double sum_nij2 = 0.0L, A = 0.0L, B = 0.0L;
-    // for (const auto& [gid, row] : contingency)
-    //     for (const auto& [lab, cnt] : row)
-    //         sum_nij2 += static_cast<long double>(comb2(cnt));
-
-    // for (const auto& [gid, a] : groupSizes) A += static_cast<long double>(comb2(a));
-    // for (const auto& [lab, b] : labelSizes) B += static_cast<long double>(comb2(b));
-
-    // const long double CN2 = static_cast<long double>(comb2(N));
-    // double ari = 0.0;
-    // if (CN2 > 0.0L) {
-    //     const long double expected = (A * B) / CN2;
-    //     const long double maxIndex = 0.5L * (A + B);
-    //     const long double denom = maxIndex - expected;
-    //     const long double numer = sum_nij2 - expected;
-    //     ari = (denom != 0.0L) ? static_cast<double>(numer / denom) : 0.0;
-    // }
-    // count.ARI = ari;
+    count.recall = (numTaxGroups == 0.0f) ? 0.0f : numMajorGroup / numTaxGroups;
 }
